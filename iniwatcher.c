749ebdbb8936c8bb31021c4c6cb8378c3b516232
Nockiro <info@nockiro.de>
2020-10-30 22:35:59 +0100

    Refactoring

--- iniwatcher.c
+++ iniwatcher.c
@@ -16,55 +16,114 @@ enum STATUS
     ENABLED
 };
 
-static LPCWSTR lpszStatus = L"Status von %ls: %ls";
-static LPCWSTR STATUS_STRING[] = {L"", L"Unknown", L"Disabled", L"Enabled"};
+struct MONRES {    
+    long X;
+    long Y;
+};
+
+struct INIPARAM {    
+    LPWSTR lpszArgFile;
+    LPWSTR lpszArgKey;
+    LPWSTR lpszArgExpectedValue;
+};
+
+static const LPCSTR lpszAppName = "IniWatcher";
+static const LPCSTR lpszTitle = "IniWatcher";
+
+static const LPCWSTR lpszStatus = L"Status von %ls: %ls";
+static const LPCWSTR STATUS_STRING[] = {L"", L"Unknown", L"Disabled", L"Enabled"};
 static const COLORREF STATUS_COLOR[] = {0, RGB(220, 220, 220), RGB(255, 0, 0), RGB(0, 255, 0)};
 static const COLORREF STATUS_BGCOLOR[] = {0, RGB(220, 220, 220), RGB(80, 0, 0), RGB(0, 80, 0)};
 
-static LPWSTR argFile;
-static LPWSTR argKey;
-static LPWSTR argExpectedValue;
-
-static long screenResX = 0;
-static long screenResY = 0;
-
-//Deklaration von WndProc
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-//Name der Applikation
-LPCSTR lpszAppName = "AppName";
-//Titelleiste der Applikation
-LPCSTR lpszTitle = "IniWatcher";
+/* Helper methods */
+struct MONRES getScreenResolution() {
+    
+    POINT aPoint;
+    LPPOINT lpPoint = &aPoint;
 
-static int lastGotStatusTime;
-enum STATUS getStatus()
+    GetCursorPos(lpPoint);
+    HMONITOR hMonitor = MonitorFromPoint(aPoint, MONITOR_DEFAULTTONEAREST);
+
+    MONITORINFO target;
+    target.cbSize = sizeof(MONITORINFO);
+    GetMonitorInfo(hMonitor, &target);
+
+    struct MONRES res = {.X = target.rcMonitor.right, .Y = target.rcMonitor.bottom};
+    return res;
+}
+
+struct INIPARAM getIniParameters()
 {
-    if (lastGotStatusTime == time(NULL))
+    int iArgs;
+    struct INIPARAM iniParam;
+    LPWSTR *aCommandline = CommandLineToArgvW(GetCommandLineW(), &iArgs);
+
+    wprintf(L"Parsing %s..\n", GetCommandLineW());
+
+    // skipping file name, hence i=1
+    for (int i = 1; i < iArgs; i += 2)
+    {
+        if (!wcscmp(aCommandline[i], L"-file"))
+        {
+            iniParam.lpszArgFile = (LPWSTR)malloc((wcslen(aCommandline[i + 1]) + 1) * sizeof(WCHAR));
+            wcsncpy(iniParam.lpszArgFile, aCommandline[i + 1], wcslen(aCommandline[i + 1]) + 1);
+        }
+        else if (!wcscmp(aCommandline[i], L"-key"))
+        {
+            iniParam.lpszArgKey = (LPWSTR)malloc((wcslen(aCommandline[i + 1]) + 1) * sizeof(WCHAR));
+            wcsncpy(iniParam.lpszArgKey, aCommandline[i + 1], wcslen(aCommandline[i + 1]) + 1);
+        }
+        else if (!wcscmp(aCommandline[i], L"-value"))
+        {
+            iniParam.lpszArgExpectedValue = (LPWSTR)malloc((wcslen(aCommandline[i + 1]) + 1) * sizeof(WCHAR));
+            wcsncpy(iniParam.lpszArgExpectedValue, aCommandline[i + 1], wcslen(aCommandline[i + 1]) + 1);
+        }
+        else
+        {
+            wprintf(L"Invalid parameter: %s - %s", aCommandline[i], aCommandline[i + 1]);
+        }
+    }
+
+    // debug output
+    wprintf(L"Watching %s for %s to be %s\n", iniParam.lpszArgFile, iniParam.lpszArgKey, iniParam.lpszArgExpectedValue);
+    LocalFree(aCommandline);
+
+    return iniParam;
+}
+
+/* Functional helper methods */
+enum STATUS getStatus(struct INIPARAM inival)
+{
+    static int iLastGotStatusTime = 0;
+
+    if (iLastGotStatusTime == time(NULL))
     {
         return NOTFETCHED;
     }
 
-    lastGotStatusTime = time(NULL);
+    iLastGotStatusTime = time(NULL);
 
-    WCHAR buffer[MAX_LINE_SIZE];
+    WCHAR sIniLineBuffer[MAX_LINE_SIZE];
     FILE *fp;
 
-    if ((fp = _wfopen(argFile, L"r")) == NULL)
+    if ((fp = _wfopen(inival.lpszArgFile, L"r")) == NULL)
     {
         perror("Failed to open file");
         return UNKNOWN;
     }
 
-    while (fgetws(buffer, MAX_LINE_SIZE, fp) != NULL)
+    while (fgetws(sIniLineBuffer, MAX_LINE_SIZE, fp) != NULL)
     {
         // if line starts with key..
-        if (!wcsncmp(buffer, argKey, wcslen(argKey)))
+        if (!wcsncmp(sIniLineBuffer, inival.lpszArgKey, wcslen(inival.lpszArgKey)))
         {
             // and then an equal sign follows..
-            if (!wcsncmp(buffer + wcslen(argKey), L"=", 1))
+            if (!wcsncmp(sIniLineBuffer + wcslen(inival.lpszArgKey), L"=", 1))
             {
                 // .. AND the value is equal ..
-                if (!wcsncmp(buffer + wcslen(argKey) + 1, argExpectedValue, wcslen(argExpectedValue)))
+                if (!wcsncmp(sIniLineBuffer + wcslen(inival.lpszArgKey) + 1, inival.lpszArgExpectedValue, wcslen(inival.lpszArgExpectedValue)))
                 {
                     return ENABLED;
                 }
@@ -81,15 +140,17 @@ enum STATUS getStatus()
     }
 
     fclose(fp);
-    LocalFree(buffer);
+    LocalFree(sIniLineBuffer);
 
     return UNKNOWN;
 }
 
-static LPWSTR lpszCurrentOutString = NULL;
-void drawStatus(enum STATUS status, HWND hWnd)
+void drawStatus(enum STATUS status, HWND hWnd, struct INIPARAM iniParam)
 {
-    if (status == NOTFETCHED) {
+    static LPWSTR lpszCurrentDisplay = NULL;
+
+    if (status == NOTFETCHED)
+    {
         return;
     }
 
@@ -98,200 +159,110 @@ void drawStatus(enum STATUS status, HWND hWnd)
 
     HDC hDC;
     PAINTSTRUCT ps;
-    COLORREF cStatusColor = STATUS_COLOR[status];
+    COLORREF crStatusColor = STATUS_COLOR[status];
     COLORREF cStatusBgColor = STATUS_BGCOLOR[status];
     LPCWSTR cStatusString = STATUS_STRING[status];
 
-    lpszCurrentOutString = malloc (MAX_LINE_SIZE * sizeof(WCHAR)) + sizeof(WCHAR);
-    _snwprintf_s(lpszCurrentOutString, MAX_LINE_SIZE, MAX_LINE_SIZE, lpszStatus, argKey, cStatusString);
+    lpszCurrentDisplay = malloc(MAX_LINE_SIZE * sizeof(WCHAR)) + sizeof(WCHAR);
+    _snwprintf_s(lpszCurrentDisplay, MAX_LINE_SIZE, MAX_LINE_SIZE, lpszStatus, iniParam.lpszArgKey, cStatusString);
 
     hDC = BeginPaint(hWnd, &ps);
 
     SetBkColor(hDC, cStatusBgColor);
-    SetTextColor(hDC, cStatusColor);
-    TextOutW(hDC, 10, 10, lpszCurrentOutString, wcslen(lpszCurrentOutString));
+    SetTextColor(hDC, crStatusColor);
+    TextOutW(hDC, 10, 10, lpszCurrentDisplay, wcslen(lpszCurrentDisplay));
 
     EndPaint(hWnd, &ps);
 
     // set window to top
-    SetWindowPos(hWnd, HWND_TOPMOST, screenResX - WND_WIDTH, screenResY - WND_HEIGHT + 10, WND_WIDTH, WND_HEIGHT, SWP_NOMOVE | SWP_NOSIZE);
+    struct MONRES res = getScreenResolution();
+    SetWindowPos(hWnd, HWND_TOPMOST, res.X - WND_WIDTH, res.Y - WND_HEIGHT + 10, WND_WIDTH, WND_HEIGHT, SWP_NOMOVE | SWP_NOSIZE);
 
     DeleteObject(hDC);
     DeleteObject(&ps);
 }
 
-void processCommandLineArgs()
-{
-    int nArgs;
-    LPWSTR *szArglist = CommandLineToArgvW(GetCommandLineW(), &nArgs);
-
-    wprintf(L"Parsing %s..\n", GetCommandLineW());
-
-    int i = 1;
-    // skipping file name..
-    for (i = 1; i < nArgs; i += 2)
-    {
-        if (!wcscmp(szArglist[i], L"-file"))
-        {
-            argFile = (LPWSTR)malloc((wcslen(szArglist[i + 1]) + 1) * sizeof(WCHAR));
-            wcsncpy(argFile, szArglist[i + 1], wcslen(szArglist[i + 1]) + 1);
-        }
-        else if (!wcscmp(szArglist[i], L"-key"))
-        {
-            argKey = (LPWSTR)malloc((wcslen(szArglist[i + 1]) + 1) * sizeof(WCHAR));
-            wcsncpy(argKey, szArglist[i + 1], wcslen(szArglist[i + 1]) + 1);
-        }
-        else if (!wcscmp(szArglist[i], L"-value"))
-        {
-            argExpectedValue = (LPWSTR)malloc((wcslen(szArglist[i + 1]) + 1) * sizeof(WCHAR));
-            wcsncpy(argExpectedValue, szArglist[i + 1], wcslen(szArglist[i + 1]) + 1);
-        }
-        else
-        {
-            wprintf(L"Invalid parameter: %s - %s", szArglist[i], szArglist[i + 1]);
-        }
-    }
-
-    wprintf(L"Watching %s for %s to be %s\n", argFile, argKey, argExpectedValue);
-
-    // Free memory allocated for CommandLineToArgvW arguments.
-    LocalFree(szArglist);
-}
-
-void cleanUp()
-{
-    free(argFile);
-    free(argKey);
-    free(argExpectedValue);
-    free(lpszCurrentOutString);
-}
-
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine, int iCmdShow)
-{   
-    HWND hWnd;     //Fensterhandle
-    MSG msg;       //Nachrichten
-    WNDCLASSEX wc; //Fensterklasse
+{
+    fflush(stdout);
+    HWND hWnd;
+    MSG msg;
+    WNDCLASSEX wc;
 
-    //Größe der Struktur WNDCLASSEX
     wc.cbSize = sizeof(WNDCLASSEX);
-    //Fensterstil
     wc.style = CS_HREDRAW | CS_VREDRAW;
-    //Prozedur für das Fenster
     wc.lpfnWndProc = WndProc;
-    //Speicher für weitere Infos reservieren
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
-    //Instanz-Handle
     wc.hInstance = hInstance;
-    //Mauscursor
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    //Icon
     wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    //Hintergrundfarbe
-    wc.hbrBackground = (HBRUSH)RGB(0,0,0);
-    //Applikationsname
+    wc.hbrBackground = (HBRUSH)RGB(0, 0, 0);
     wc.lpszClassName = lpszAppName;
-    //Menüname
     wc.lpszMenuName = lpszAppName;
-    //Icon neben dem Fenstertitel
     wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
-    //Fensterklasse registrieren
-    if (RegisterClassEx(&wc) == 0)
-        return 0;
+    if (RegisterClassEx(&wc) == 0) return 0;
 
-    MONITORINFO target;
-    target.cbSize = sizeof(MONITORINFO);
+    struct MONRES res = getScreenResolution();
 
-    POINT aPoint;
-    LPPOINT lpPoint = &aPoint;
-
-    GetCursorPos(lpPoint);
-    HMONITOR hMonitor = MonitorFromPoint(aPoint, MONITOR_DEFAULTTONEAREST);
-
-    GetMonitorInfo(hMonitor, &target);
-
-    screenResX = target.rcMonitor.right;
-    screenResY = target.rcMonitor.bottom;
-
-    //Fenster erstellen
-    hWnd = CreateWindowEx( //Fensterstil
-        WS_EX_LAYERED| WS_EX_TOOLWINDOW,
-        //Name Fensterklasse
+    hWnd = CreateWindowEx(
+        WS_EX_LAYERED | WS_EX_TOOLWINDOW,
         lpszAppName,
-        //Fenstertitel
         lpszTitle,
-        //Art des Fensters
         WS_POPUP,
-        //x, y
-        screenResX - WND_WIDTH, screenResY - WND_HEIGHT + 10,
-        //Fensterbreite
-        WND_WIDTH,
-        //Fensterhöhe
-        WND_HEIGHT,
-        //Fenster-Handle von übergeordnetem Fenster
+        res.X - WND_WIDTH, res.Y - WND_HEIGHT + 10,
+        WND_WIDTH, WND_HEIGHT,
         NULL,
-        //Fenster Menü
         NULL,
-        //Instanz der Anwendung aus WinMain()
         hInstance,
-        //für MDI-Anwendungen
         NULL);
 
-    if (hWnd == NULL)
-        return 0;
+    if (hWnd == NULL) return 0;
 
-    processCommandLineArgs();
-    SetLayeredWindowAttributes(hWnd, RGB(0,0,0), (BYTE)0, LWA_COLORKEY);
-    
-    //Anzeigestatus
+    struct INIPARAM iniParam = getIniParameters();
+
+    // make window itself transparent since we only need the control on it to be displayed
+    SetLayeredWindowAttributes(hWnd, RGB(0, 0, 0), (BYTE)0, LWA_COLORKEY);
+
     ShowWindow(hWnd, iCmdShow);
-
-    //Fenster zeichnen
     UpdateWindow(hWnd);
 
-    //Nachrichten aus der Warteschlange abfragen
     while (GetMessage(&msg, NULL, 0, 0))
     {
-        drawStatus(getStatus(), hWnd);
+        // always draw status on window update - e.g. if hovering over it with the mouse
+        drawStatus(getStatus(iniParam), hWnd, iniParam);
 
-        //für virt. Tastaturcode
         TranslateMessage(&msg);
-        //Nachrichten an die Prozedur senden
         DispatchMessage(&msg);
     }
+    
     return msg.wParam;
 }
 
-//Prozedur für das Hauptfenster
 LRESULT CALLBACK WndProc(HWND hWnd, UINT umsg, WPARAM wParam, LPARAM lParam)
 {
-    LRESULT hit; //Zwischenspeicher fuer manuelles Hit-Handling
+    // cache for manual hit handling
+    LRESULT hit;
     RECT rect;
 
-    //Nachrichten auswerten
     switch (umsg)
     {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        cleanUp();
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        case WM_ERASEBKGND:
+            GetClientRect(hWnd, &rect);
+            FillRect((HDC)wParam, &rect, CreateSolidBrush(RGB(0, 0, 0)));
 
-        return 0;
-    case WM_CREATE:
-        break;
-    case WM_ERASEBKGND:    
-        GetClientRect(hWnd, &rect);
-        FillRect((HDC)wParam, &rect, CreateSolidBrush(RGB(0, 0, 0)));
-        break;
+            break;
+        case WM_NCHITTEST:
+            hit = DefWindowProc(hWnd, umsg, wParam, lParam);
+            if (hit == HTCLIENT)
+                hit = HTCAPTION;
 
-    case WM_NCHITTEST:
-        hit = DefWindowProc(hWnd, umsg, wParam, lParam);
-        if (hit == HTCLIENT)
-            hit = HTCAPTION;
-        return hit;
+            return hit;
     }
 
-    //falls Nachricht nicht behandelt wurde von
-    //Windows bearbeiten lassen
     return DefWindowProc(hWnd, umsg, wParam, lParam);
 }
