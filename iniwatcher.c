bbef02c45390be79628bf7a36282469aac83c623
Nockiro <info@nockiro.de>
2020-11-01 10:23:38 +0100

    added keeping the window position over restart

--- iniwatcher.c
+++ iniwatcher.c
@@ -1,3 +1,4 @@
+#include <shlwapi.h>
 #include <windows.h>
 #include <strsafe.h>
 #include <shellapi.h>
@@ -16,12 +17,8 @@ enum STATUS
     ENABLED
 };
 
-struct MONRES {    
-    long X;
-    long Y;
-};
-
-struct INIPARAM {    
+struct INIPARAM
+{
     LPWSTR lpszArgFile;
     LPWSTR lpszArgKey;
     LPWSTR lpszArgExpectedValue;
@@ -32,14 +29,14 @@ static const LPCSTR lpszTitle = "IniWatcher";
 
 static const LPCWSTR lpszStatus = L"Status von %ls: %ls";
 static const LPCWSTR STATUS_STRING[] = {L"", L"Unknown", L"Disabled", L"Enabled"};
-static const COLORREF STATUS_COLOR[] = {0, RGB(220, 220, 220), RGB(255, 0, 0), RGB(0, 255, 0)};
-static const COLORREF STATUS_BGCOLOR[] = {0, RGB(220, 220, 220), RGB(80, 0, 0), RGB(0, 80, 0)};
+static const COLORREF STATUS_COLOR[] = {0, RGB(240, 240, 240), RGB(255, 0, 0), RGB(0, 255, 0)};
+static const COLORREF STATUS_BGCOLOR[] = {0, RGB(100, 100, 100), RGB(80, 0, 0), RGB(0, 80, 0)};
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 /* Helper methods */
-struct MONRES getScreenResolution() {
-    
+POINTS getScreenResolution()
+{
     POINT aPoint;
     LPPOINT lpPoint = &aPoint;
 
@@ -50,7 +47,7 @@ struct MONRES getScreenResolution() {
     target.cbSize = sizeof(MONITORINFO);
     GetMonitorInfo(hMonitor, &target);
 
-    struct MONRES res = {.X = target.rcMonitor.right, .Y = target.rcMonitor.bottom};
+    POINTS res = {.x = target.rcMonitor.right, .y = target.rcMonitor.bottom};
     return res;
 }
 
@@ -91,6 +88,44 @@ struct INIPARAM getIniParameters()
     LocalFree(aCommandline);
 
     return iniParam;
+}
+
+void GetConfigPath(LPWSTR lpszTempPath)
+{
+    GetTempPathW(MAX_PATH, lpszTempPath);
+    PathCombineW(lpszTempPath, lpszTempPath, L"iniwatcher.dat");
+}
+
+POINTS getWindowCoordinates()
+{
+    // file handle for permanent storage of the coordinates
+    FILE *tmpFile;
+    WCHAR lpszTempPath[MAX_PATH];
+    WCHAR lpszLineBuffer[MAX_LINE_SIZE];
+
+    POINTS retValue;
+
+    GetConfigPath(lpszTempPath);
+
+    if ((tmpFile = _wfopen(lpszTempPath, L"r")) != NULL)
+    {
+        fgetws(lpszLineBuffer, MAX_LINE_SIZE, tmpFile);
+        retValue.x = StrToIntW(lpszLineBuffer);
+
+        fgetws(lpszLineBuffer, MAX_LINE_SIZE, tmpFile);
+        retValue.y = StrToIntW(lpszLineBuffer);
+    }
+    else
+    {
+        retValue = getScreenResolution();
+        retValue.x -= WND_WIDTH;
+        retValue.y -= WND_HEIGHT;
+        retValue.y += 10;
+    }
+
+    fclose(tmpFile);
+
+    return retValue;
 }
 
 /* Functional helper methods */
@@ -149,10 +184,7 @@ void drawStatus(enum STATUS status, HWND hWnd, struct INIPARAM iniParam)
 {
     static LPWSTR lpszCurrentDisplay = NULL;
 
-    if (status == NOTFETCHED)
-    {
-        return;
-    }
+    if (status == NOTFETCHED) return;
 
     // invalidate window to get a new label to be visible
     RedrawWindow(hWnd, NULL, NULL, RDW_ERASE | RDW_INVALIDATE);
@@ -163,7 +195,7 @@ void drawStatus(enum STATUS status, HWND hWnd, struct INIPARAM iniParam)
     COLORREF cStatusBgColor = STATUS_BGCOLOR[status];
     LPCWSTR cStatusString = STATUS_STRING[status];
 
-    lpszCurrentDisplay = malloc(MAX_LINE_SIZE * sizeof(WCHAR)) + sizeof(WCHAR);
+    lpszCurrentDisplay = malloc(MAX_LINE_SIZE * sizeof(WCHAR) + sizeof(WCHAR));
     _snwprintf_s(lpszCurrentDisplay, MAX_LINE_SIZE, MAX_LINE_SIZE, lpszStatus, iniParam.lpszArgKey, cStatusString);
 
     hDC = BeginPaint(hWnd, &ps);
@@ -175,16 +207,17 @@ void drawStatus(enum STATUS status, HWND hWnd, struct INIPARAM iniParam)
     EndPaint(hWnd, &ps);
 
     // set window to top
-    struct MONRES res = getScreenResolution();
-    SetWindowPos(hWnd, HWND_TOPMOST, res.X - WND_WIDTH, res.Y - WND_HEIGHT + 10, WND_WIDTH, WND_HEIGHT, SWP_NOMOVE | SWP_NOSIZE);
+    POINTS coord = getWindowCoordinates();
+    SetWindowPos(hWnd, HWND_TOPMOST, coord.x, coord.y, WND_WIDTH, WND_HEIGHT, SWP_NOMOVE | SWP_NOSIZE);
 
     DeleteObject(hDC);
     DeleteObject(&ps);
 }
 
+/* Window functions */
+
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine, int iCmdShow)
 {
-    fflush(stdout);
     HWND hWnd;
     MSG msg;
     WNDCLASSEX wc;
@@ -197,28 +230,30 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLi
     wc.hInstance = hInstance;
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
     wc.hbrBackground = (HBRUSH)RGB(0, 0, 0);
     wc.lpszClassName = lpszAppName;
     wc.lpszMenuName = lpszAppName;
-    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
-    if (RegisterClassEx(&wc) == 0) return 0;
+    if (RegisterClassEx(&wc) == 0)
+        return 0;
 
-    struct MONRES res = getScreenResolution();
+    POINTS coord = getWindowCoordinates();
 
     hWnd = CreateWindowEx(
         WS_EX_LAYERED | WS_EX_TOOLWINDOW,
         lpszAppName,
         lpszTitle,
         WS_POPUP,
-        res.X - WND_WIDTH, res.Y - WND_HEIGHT + 10,
+        coord.x, coord.y,
         WND_WIDTH, WND_HEIGHT,
         NULL,
         NULL,
         hInstance,
         NULL);
 
-    if (hWnd == NULL) return 0;
+    if (hWnd == NULL)
+        return 0;
 
     struct INIPARAM iniParam = getIniParameters();
 
@@ -236,32 +271,50 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLi
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-    
+
     return msg.wParam;
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT umsg, WPARAM wParam, LPARAM lParam)
 {
+    RECT rect;
     // cache for manual hit handling
     LRESULT hit;
-    RECT rect;
+    // memory storage for current window coordinates
+    WINDOWPLACEMENT wpm;
+    // file handle for permanent storage of the coordinates
+    FILE *tmpFile;
+    WCHAR lpszTempPath[MAX_PATH];
 
     switch (umsg)
     {
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            return 0;
-        case WM_ERASEBKGND:
-            GetClientRect(hWnd, &rect);
-            FillRect((HDC)wParam, &rect, CreateSolidBrush(RGB(0, 0, 0)));
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    case WM_ERASEBKGND:
+        GetClientRect(hWnd, &rect);
+        FillRect((HDC)wParam, &rect, CreateSolidBrush(RGB(0, 0, 0)));
 
-            break;
-        case WM_NCHITTEST:
-            hit = DefWindowProc(hWnd, umsg, wParam, lParam);
-            if (hit == HTCLIENT)
-                hit = HTCAPTION;
+        break;
+    case WM_NCHITTEST:
+        hit = DefWindowProc(hWnd, umsg, wParam, lParam);
+        if (hit == HTCLIENT)
+            hit = HTCAPTION;
 
-            return hit;
+        return hit;
+    case WM_EXITSIZEMOVE:    
+        // store window position for after restart
+        GetWindowPlacement(hWnd, &wpm);
+        GetConfigPath(lpszTempPath);
+
+        if ((tmpFile = _wfopen(lpszTempPath, L"w")) != NULL)
+        {
+            fprintf(tmpFile, "%ld\n", wpm.rcNormalPosition.left);
+            fprintf(tmpFile, "%ld\n", wpm.rcNormalPosition.top);
+        }
+
+        fclose(tmpFile);
+        break;
     }
 
     return DefWindowProc(hWnd, umsg, wParam, lParam);
